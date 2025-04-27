01bee9b37b1cab804c970b2ebf27792a5f642bd7
ZeeshanAli13 <devzeeshanali2@gmail.com>
2025-04-27 21:27:13 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
